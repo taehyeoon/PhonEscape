0676b50ec4ee3133c3b690f1bcb8991460937e72
taehyeon <tedxpem@naver.com>
2023-04-22 01:11:24 +0900

    intro에 관련된 모든 정보 GameData를 이용
    터치할 때마다 미리 저장된 스토리 진행
    intro -> easyRoom 임시 버튼 삭제

--- Assets/Scripts/Canvas/IntroCanvas.cs
+++ Assets/Scripts/Canvas/IntroCanvas.cs
@@ -5,40 +5,66 @@ using System.Collections;
 using System;
 
 public class IntroCanvas : MonoBehaviour
-{
-    
-    [SerializeField] GameObject[] textLayouts;
+{    
+    [SerializeField] TMP_Text hintTextLayout;
+    [SerializeField] TMP_Text[] textLayouts;
     private string[] texts;
     private int curTextIndex;
-    private bool isTouchEnable;
     private float fadeHintTime;
     private float fadeTextTime;
-    public GameObject hintText;
 
-    public Button tempPlayButton;
+    private bool isCoroutineRunning;
+    private bool isTouchEnable;
 
     private void Awake()
     {
+        TextValidCheck();        
         init();
-        TextValidCheck();
     }
 
     private void Start()
     {
-        StartCoroutine(FadeText(hintText));
+        StartCoroutine(FadeInOutText(hintTextLayout));
     }
 
     private void Update()
     {
-        //if (Input.GetKey(KeyCode.Space) && isTouchEnable)
-        //{
-        //    if(curTextIndex < texts.Length)
-        //    {
-        //        isTouchEnable = false;
-        //        curTextIndex++; // outofIndex Exception
-        //        Debug.Log("clicked");
-        //    }
-        //}
+        isTouchEnable = !isCoroutineRunning;
+
+        if (!isTouchEnable)
+            return;
+
+        if (!CheckTouched())
+            return;
+
+        if (CheckedIntroEnd())
+        {
+            SceneLoader.LoadScene(GameData.data.selectedLevelScene.ToString());
+        }
+
+        if (curTextIndex < texts.Length)
+        {
+            if (curTextIndex != 0)
+            {
+                StartCoroutine(FadeOutText(textLayouts[curTextIndex - 1]));
+            }
+            StartCoroutine(FadeInText(textLayouts[curTextIndex]));
+            curTextIndex++;
+        }   
+    }
+
+    private bool CheckTouched()
+    {
+        return Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Space);
+    }
+
+    private bool CheckedIntroEnd()
+    {
+        TMP_Text lastText = textLayouts[textLayouts.Length - 1];
+        if(lastText.gameObject.activeSelf && lastText.GetComponent<CanvasGroup>().alpha == 1)
+            return true;
+        else
+            return false;
     }
 
     private void OnDisable()
@@ -49,64 +75,95 @@ public class IntroCanvas : MonoBehaviour
     private void init()
     {
         curTextIndex = 0;
+        isCoroutineRunning = false;
         isTouchEnable = true;
-        fadeHintTime = 0.5f;
-        fadeTextTime = 0.5f;
 
         texts = GameData.data.introTextList;
-        foreach(GameObject obj in textLayouts)
+        hintTextLayout.text = GameData.data.hintText;
+        fadeHintTime = GameData.data.hintFadeInterval;
+        fadeTextTime = GameData.data.textFadeInterval;
+
+        // Set text active false
+        hintTextLayout.gameObject.SetActive(false);
+        for(int i =0; i< texts.Length; i++)
         {
-            obj.SetActive(false);
+            textLayouts[i].text = texts[i];
+            textLayouts[i].gameObject.SetActive(false);
         }
-
-        // Temp
-        tempPlayButton.onClick.AddListener(() =>
-        {
-            SceneLoader.LoadScene(EScenes.EasyRoom.ToString());
-        });
-
     }
 
     private void TextValidCheck()
     {
-        if(texts.Length != textLayouts.Length)
-        {
-            Debug.LogError("The number of elements in textLayouts and texts should be the same");
-        }
+        string[] listForChecking = GameData.data.introTextList;
 
-        foreach(string text in texts)
+        foreach(string text in listForChecking)
         {
             if(text == "")
             {
                 Debug.LogError("intro text cannot be null");
             }
         }
+
+        if(listForChecking.Length != textLayouts.Length)
+        {
+            Debug.LogError("The number of elements in textLayouts and texts should be the same");
+        }
     }
 
     // Repeat fadeIn, fadeOut indefinitely
-    IEnumerator FadeText(GameObject hintText)
+    IEnumerator FadeInOutText(TMP_Text hintText)
     {
-        hintText.SetActive(true);
+        hintText.gameObject.SetActive(true);
         CanvasGroup canvasGroup = hintText.GetComponent<CanvasGroup>();
         while (true)
         {
             canvasGroup.alpha = 0;
-            yield return new WaitForSeconds(fadeHintTime);
 
             while (canvasGroup.alpha < 1)
             {
                 canvasGroup.alpha += Time.deltaTime / fadeHintTime;
                 yield return null;
             }
-            yield return new WaitForSeconds(fadeHintTime);
 
             while (canvasGroup.alpha > 0)
             {
                 canvasGroup.alpha -= Time.deltaTime / fadeHintTime;
                 yield return null;
             }
-            yield return new WaitForSeconds(fadeHintTime);
         }
     }
 
+    IEnumerator FadeInText(TMP_Text target)
+    {
+        isCoroutineRunning = true;
+
+        target.gameObject.SetActive(true);
+        CanvasGroup canvasGroup = target.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 0;
+
+        while(canvasGroup.alpha < 1)
+        {
+            canvasGroup.alpha += Time.deltaTime / fadeTextTime;
+            yield return null;
+        }
+
+        isCoroutineRunning = false;
+    }
+
+    IEnumerator FadeOutText(TMP_Text target)
+    {
+        isCoroutineRunning = true;
+
+        target.gameObject.SetActive(true);
+        CanvasGroup canvasGroup = target.GetComponent<CanvasGroup>();
+        canvasGroup.alpha = 1;
+
+        while (canvasGroup.alpha > 0)
+        {
+            canvasGroup.alpha -= Time.deltaTime / fadeTextTime;
+            yield return null;
+        }
+
+        isCoroutineRunning = false;
+    }
 }
--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -4,15 +4,25 @@ public class GameData : MonoBehaviour
 {
     public static GameData data;
 
+    [Header("Player Data")]
     public string playreName;
+    [Tooltip("Player speed")]
     [Range(0, 15)] public float speed;
     [Range(0, 5)] public float distanceToWall;
     [Range(0, 5)] public float scanRange;
-    [Range(0, 1)] public float joystickRangeMin; // The player moves when the stick is further than this value from the center
+    [Tooltip("The player moves when the stick is further than this value from the center")]
+    [Range(0, 1)] public float joystickRangeMin;
 
-    public string[] introTextList;
-
+    [Header("SelectName Scene")]
     public EScenes selectedLevelScene;
+
+    [Header("Intro Scene")]
+    public string[] introTextList;
+    public string hintText;
+    [Tooltip("The hint phrase appears slowly for a set amount of time, and then slowly disappears.")]
+    [Range(0.1f, 5f)] public float hintFadeInterval;
+    [Tooltip("Indicates the time when the text appears")]
+    [Range(0.1f, 5f)] public float textFadeInterval;
 
     private void Awake()
     {
