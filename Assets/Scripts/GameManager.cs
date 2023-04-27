61ffe3154ef17a6bcbd81fb2e4ed0648f9752a00
taehyeon <tedxpem@naver.com>
2023-04-27 17:23:48 +0900

    Code 브랜치 생성
    GameManager와 Data를 DontDestroyOnLoad Child로 배치
    DontDestroyOnLoad 코드 삭제

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,13 +3,6 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     private static GameManager instance;
-
-    private void Awake()
-    {
-        DontDestroyOnLoad(this);
-    }
-
-
     public static GameManager GetInstance()
     {
         if (instance == null)
