3dd6003185628723d900143d6fcba0019bf77d19
taehyeon <tedxpem@naver.com>
2023-05-08 15:36:10 +0900

    퀴즈 부분 구현
    현재 10초동안 아무런 선택을 하지 않으면 바로 nomal mode로 진입

--- Assets/Scripts/Canvas/QuizCanvas.cs
+++ Assets/Scripts/Canvas/QuizCanvas.cs
@@ -1,3 +1,5 @@
+using System;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,6 +7,15 @@ public class QuizCanvas : MonoBehaviour
 {
     public Button temp_success;        
     public Button temp_fail;
+
+    private Sprite[] snsImages;
+    private Sprite nonSnsImage;
+
+    public float timeLimit;
+    public float remainingTime;
+
+    public TMP_Text timeText;
+    public Button[] optionBtns;
 
     private void Awake()
     {
@@ -19,5 +30,39 @@ public class QuizCanvas : MonoBehaviour
             GameData.data.selectedLevelScene = EScenes.HardRoom;
             SceneLoader.LoadScene(EScenes.Intro.ToString());
         });
+        
+        optionBtns[0].image.sprite = Resources.Load<Sprite>($"Sprites/snsImage0");
+        optionBtns[1].image.sprite = Resources.Load<Sprite>($"Sprites/snsImage1");
+        optionBtns[2].image.sprite = Resources.Load<Sprite>($"Sprites/snsImage2");
+        optionBtns[3].image.sprite = Resources.Load<Sprite>($"Sprites/nonSnsImage");
+
+        for (int i = 0; i < 3; i++)
+        {
+            optionBtns[i].onClick.AddListener(() =>
+            {
+                GameData.data.selectedLevelScene = EScenes.EasyRoom;
+                SceneLoader.LoadScene(EScenes.Intro.ToString());
+            });
+        }
+        
+        optionBtns[3].onClick.AddListener(() =>
+        {
+            GameData.data.selectedLevelScene = EScenes.HardRoom;
+            SceneLoader.LoadScene(EScenes.Intro.ToString());
+        });
+        
+        
+        remainingTime = timeLimit;
+    }
+
+    private void Update()
+    {
+        if (remainingTime <= 0)
+        {
+            GameData.data.selectedLevelScene = EScenes.EasyRoom;
+            SceneLoader.LoadScene(EScenes.Intro.ToString());
+        }
+        remainingTime -= Time.deltaTime;
+        timeText.text = $"Time : {remainingTime.ToString($"F1")} s";
     }
 }
