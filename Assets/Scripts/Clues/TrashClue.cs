0790b09912557a09c0c1613f781c6ec0d3b4648b
taehyeon <tedxpem@naver.com>
2023-05-27 15:50:45 +0900

    쓰레기 색 변경(검정 > 진보라)
    sprite meta파일 추가
    쓰레기clueNumber 라이트 범위 확대

--- Assets/Scripts/Clues/TrashClue.cs
+++ Assets/Scripts/Clues/TrashClue.cs
@@ -15,6 +15,7 @@ public class TrashClue : MonoBehaviour
     public static Action PlusFullnessLevel;
     public static Action<GameObject> GetTrash;
 
+    public GameObject clueNumber;
     public Player player;
     private void Awake()
     {
@@ -32,6 +33,7 @@ public class TrashClue : MonoBehaviour
         if (fullnessLevel == maxFullnessLevel)
         {
             hintLight.SetActive(true);
+            clueNumber.GetComponent<SpriteRenderer>().sortingLayerName = "Number";
         }
     }
 
